0d503e3a4d6470fc6a446e7844ab5219bcf9ed38
Yayeh <85830416+YayehK@users.noreply.github.com>
2021-11-25 21:32:24 +0100

    If applied, this commit will have updated the contents in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
+/*This project is to building a Pokédex application that displays a list of Pokémon and
+ allows the user to view details for each of them*/
+
+alert('Hello World and Mwaramutse Blaise :)');
 let favoriteFood = Enjera;
 documet.write(favoriteFood);
