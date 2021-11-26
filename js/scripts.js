b042adb6f8525cee75d6f6c9702199cc852f833d
Yayeh <85830416+YayehK@users.noreply.github.com>
2021-11-26 14:10:39 +0100

    If applied, this commit will have added array objects to the variable pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 /*This project is to building a Pokédex application that displays a list of Pokémon and
  allows the user to view details for each of them*/
-
-alert('Hello World and Mwaramutse Blaise :)');
-let favoriteFood = Enjera;
-documet.write(favoriteFood);
+ let pokemonList = [
+   {name: 'Wartortle', height: 1, types:'Water'},
+   {name: 'Metapod', height: 0.7, types:'bug'},
+   {name: 'Charmander', height: 0.6, types:'fire'},
+   {name: 'Ghost', height: 0.5, types:'ghost'},
+ ];
