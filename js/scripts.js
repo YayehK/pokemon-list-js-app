02d1f0a7d01480185cbcb67d33efe2283281a714
Yayeh <85830416+YayehK@users.noreply.github.com>
2021-11-30 17:24:02 +0100

    If applied, this commit will have printed Pokémon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
-/*This project is to building a Pokédex application that displays a list of Pokémon and
- allows the user to view details for each of them*/
- let pokemonList = [
-   {name: 'Wartortle', height: 1, types:['Water']},
-   {name: 'Metapod', height: 0.7, types:['bug']},
-   {name: 'Charmander', height: 0.6, types:['fire']},
-   {name: 'Ghost', height: 0.5, types:['ghost']},
- ];
+/*This project is to build a Pokédex application that displays a list of Pokémon and
+allows the user to view details for each of them*/
+let pokemonList = [
+  {
+    name: 'Wartortle',
+    height: 1,
+    types:['Water']},
+    {
+      name: 'Metapod',
+      height: 0.7,
+      types:['bug']
+    },
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types:['fire']
+    },
+    {
+      name: 'Ghost',
+      height: 0.5,
+      types:['ghost']
+    },
+  ];
+  // The following loop will write the name and height of all Pokemons on website DOM
+  // The else loop checks if the height is above a 1.0 (big) value
+
+  for (let i=0; i<pokemonList.length; i++) {
+    if (pokemonList[i].height < 1.0) {
+      document.write(pokemonList[i].name + '(height:  '+ pokemonList[i].height +')' +'<br>');
+    } else {
+      document.write(pokemonList[i].name + '(height:  '+ pokemonList[i].height +')' + " - Wow, that's big!" +'<br>');
+    }
+  }
